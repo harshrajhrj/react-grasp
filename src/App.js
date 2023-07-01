d10d399d6e8903d93f4086e278f08b9f10336884
rajharshcr7 <rajharsh.footballer@gmail.com>
2023-07-01 11:34:43 +0530

    --stage1, completed creating expense-items

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import './App.css';
-import './Styling/li.css';
-import Tasks from './components/Tasks';
-import Header from './components/Header';
-import Footer from './components/Footer';
-import AddTask from './components/AddTask';
-import { useState } from 'react';
+import ExpenseItem from './components/ExpenseItem';
 
 function App() {
 
-  const [tasks, setTasks] = useState([]);
+  var expense_items = [
+    {
+      id: 1,
+      date: new Date(2020, 8, 14),
+      title: 'Toilet paper',
+      amount: 94.12
+    },
+    {
+      id: 2,
+      date: new Date(2021, 3, 12),
+      title: 'New TV',
+      amount: 799.49
+    },
+    {
+      id: 3,
+      date: new Date(2021, 3, 28),
+      title: 'Car insurance',
+      amount: 294.67
+    },
+    {
+      id: 4,
+      date: new Date(2021, 6, 12),
+      title: 'New desk(wooden)',
+      amount: 450
+    },
+  ]
   return (
     <div className="App">
-      <Header />
-      <main>
-        <AddTask tasks={tasks} setTasks={setTasks} />
-        <Tasks tasks={tasks} setTasks={setTasks} />
-      </main>
-      <Footer />
+      <h2>Let's get started</h2>
+      {
+        expense_items.map((item) => (
+          <ExpenseItem key={item.id} date={item.date} title={item.title} amount={item.amount} />
+        ))
+      }
     </div>
   );
 }
