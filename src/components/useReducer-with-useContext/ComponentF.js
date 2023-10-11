86d2193ebbc61fb23ebf88b647f690c42e8fe0ec
rajharshcr7 <rajharsh.footballer@gmail.com>
2023-10-11 23:31:16 +0530

    added new line

--- src/components/useReducer-with-useContext/ComponentF.js
+++ src/components/useReducer-with-useContext/ComponentF.js
@@ -9,6 +9,7 @@ export const ComponentF = () => {
             <button onClick={() => countContext.countDispatch('increment')}>Increment</button>
             <button onClick={() => countContext.countDispatch('decrement')}>Decrement</button>
             <button onClick={() => countContext.countDispatch('reset')}>Reset</button>
+            
         </div>
     )
 }
